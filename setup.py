c7aea1e90ae85d1d54b1293fa977984d6564e4fd
Sabu Siyad <sabu.siyad.p@gmail.com>
2019-11-17 15:23:10 +0530

    Upload to PyPI

--- setup.py
+++ setup.py
@@ -1,21 +1,45 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+LONG_DESC = """
+## Usage
+```
+getpalette [-h] [-c COUNT] [-s] image
+
+positional arguments:
+  image
+
+optional arguments:
+  -c COUNT, --count COUNT
+                        Number of colors to find
+  -s, --show            Show/Hide hexadecimal color values
+  -h, --help            show this help message and exit
+```
+
+## Output
+The tool will output two files into the main folder your image was initially in. The first file will be named IMAGE_palette and will contain just the color palette. The second file will be called IMAGE_with_palette and this will contain the original image with the palette below it.
+"""
 
 setup(
-    name='getpalette',
-    version='1.0.0',
-    packages=['getpalette'],
+    name="getpalette",
+    version="1.0.2",
+    description="Get color palette from images",
+    long_description = LONG_DESC,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ssiyad/getpalette",
+    author="Sabu Siyad",
+    author_email="sabu.siyad.p@gmail.com",
+    license="MIT",
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Pillow==6.2.1',
         'matplotlib==3.0.3',
         'scipy==1.3.1',
-        'pandas==0.24.2',
-        'click==6.7'
+        'pandas==0.24.2'
     ],
     entry_points={
-        'console_scripts': [
-            'getpalette = getpalette.__main__:main',
+        "console_scripts": [
+            "getpalette = getpalette.__main__:main",
         ]
     },
 )
-
